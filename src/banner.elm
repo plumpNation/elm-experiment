78d68e814e824f19a6223c7131700e37b14d0b9d
Gavin King <gavin.king@itslearning.com>
2017-04-17 13:27:18 +0200

    Starting again with the demo project

--- src/banner.elm
+++
@@ -1,7 +0,0 @@
-module Banner exposing (Banner)
-
-import Html exposing (section, text)
-import Html.Attributes exposing (class)
-
-main =
-  section [class "banner"] [text "I am banner"]
--- src/content.elm
+++
@@ -1,9 +0,0 @@
-port module Content exposing (Content)
-
-import Html exposing (section, text)
-import Html.Attributes exposing (class)
-
-port doit : String -> Cmd msg
-
-main =
-  section [class "content"] [text "I am content"]
