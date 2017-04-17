f687a176cca47ade1de84321e39fb6bc6568d520
Gavin King <gavin.king@itslearning.com>
2017-04-17 15:05:12 +0200

    Created initial model and component views for player list

--- src/Models.elm
+++ src/Models.elm
@@ -2,4 +2,22 @@ module Models exposing (..)
 
 
 type alias Model =
+    { players : List Player
+    }
+
+
+initialModel : Model
+initialModel =
+    { players = [ Player "1" "Sam" 1 ]
+    }
+
+
+type alias PlayerId =
     String
+
+
+type alias Player =
+    { id : PlayerId
+    , name : String
+    , level : Int
+    }
