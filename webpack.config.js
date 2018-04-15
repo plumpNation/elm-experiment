959231549858ce16b5cfb7db511d3c3c1d468be8
Gavin King <gavin.king@itslearning.com>
2018-04-15 16:07:34 +0200

    Upgrade deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,64 @@
-var path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: {
-    app: [
-      './src/index.js'
-    ]
-  },
+    mode: 'development',
 
-  output: {
-    path: path.resolve(__dirname + '/dist'),
-    filename: '[name].js',
-  },
+    entry: {
+        app: [
+            './src/index.js'
+        ]
+    },
 
-  module: {
-    rules: [
-      {
-        test:    /\.html$/,
-        exclude: /node_modules/,
-        loader:  'file-loader?name=[name].[ext]',
-      },
-      {
-        test:    /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack-loader?verbose=true&warn=true',
-      },
+    output: {
+        path: path.resolve(__dirname + '/dist'),
+        filename: '[name].js',
+    },
 
-      // STYLES
-      {
-          test: /\.(css|scss)$/,
-          use: [
-              'style-loader',
-              'css-loader',
-          ]
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
-      },
-    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader:    'file-loader?name=[name].[ext]',
+            },
 
-    noParse: /\.elm$/,
-  },
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                use: {
+                    loader: 'elm-webpack-loader',
+                    options: {
+                        verbose: true,
+                        warn: true
+                    }
+                }
+            },
 
-  devServer: {
-    inline: true,
-    stats: { colors: true },
-  },
+            // STYLES
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
 
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+            },
 
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader',
+            }
+        ],
+
+        noParse: /\.elm$/,
+    },
+
+    devServer: {
+        inline: true,
+        stats: { colors: true },
+    }
 };
